9fb3d3615eff9f53d28e3e30cdefe886ed0ec8ae
Arrkot <arkotnn@gmail.com>
2023-08-05 16:00:00 +0300

    lesson_8

--- app_advertisements/admin.py
+++ app_advertisements/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Advertisements
 # Register your models here.
+
+class AdvertisementsAdmin(admin.ModelAdmin):
+    # list_display = ['id', 'title', 'description', 'price', 'created_date', 'update_date', 'auction']
+    list_display = ['id', 'title', 'description', 'price', 'created_date', 'updated_date', 'auction']
+    list_filter = ['auction', 'create_at']
+
+admin.site.register(Advertisements, AdvertisementsAdmin)
+
--- app_advertisements/models.py
+++ app_advertisements/models.py
@@ -1,14 +1,15 @@
 from django.db import models
-
+from django.contrib import admin
 # Create your models here.
 
 class Advertisements(models.Model):
+
     title = models.CharField("Заголовок", max_length=128)
     description = models.TextField('Описание')
     price = models.DecimalField("Цена", max_digits=10, decimal_places=2)
     auction = models.BooleanField("Торг", help_text="Отметьте если торг уместен")
     create_at = models.DateTimeField( auto_now_add=True) #"Датта создания",
-    update_at = models.DateField( auto_now=True) #"Датта обновления",
+    update_at = models.DateTimeField(auto_now=True) #"Датта обновления",
     # uuu = models.CharField('uuu', max_length=22, default='')
 
     def __str__(self):
@@ -16,5 +17,31 @@ class Advertisements(models.Model):
 
     class Meta:
         db_table = "Advertisements"
+
+    @admin.display(description='Дата создания')
+    def created_date(self):
+        from django.utils import timezone
+        from django.utils.html import format_html
+        if self.create_at.date() == timezone.now().date():
+            created_time = self.create_at.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style="color: green; font-weight: bold">'
+                'Сегодня в {}</span>', created_time
+            )
+        return self.create_at.strftime('%d.%m.%Y в %H:%M:%S')
+
+
+    @admin.display(description='Дата обновления')
+    def updated_date(self):
+        from django.utils import timezone
+        from django.utils.html import format_html
+        print(self.update_at)
+        if self.update_at.date() == timezone.now().date():
+            updated_time = self.update_at.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style="color: blue; font-weight: bold">'
+                'Сегодня в {}</span>', updated_time
+            )
+        return self.update_at.strftime('%d.%m.%Y в %H:%M:%S')
 
 
